7ce395e33e85ba98758b75ec6ce464c1bad6fc11
dinhnguyen888 <nguyenphucdinh89@gmail.com>
2025-03-18 21:48:47 +0700

    refactor: room (conversation) api endpoint

---
+++ BackendMultiChat/Controllers/RoomController.cs
@@ -0,0 +1,68 @@
+﻿using BackendMultiChat.Dtos;
+using BackendMultiChat.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace BackendMultiChat.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomController : ControllerBase
+    {
+        private readonly IRoomService _roomService;
+        public RoomController(IRoomService roomService)
+        {
+            _roomService = roomService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRooms()
+        {
+            try
+            {
+                var rooms = await _roomService.GetAllRooms();
+                return Ok(rooms);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500, new { message = $"An error occurred: {ex.Message}" });
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewRoom(RoomCreateDto dto)
+        {
+            try
+            {
+                var result = await _roomService.CreateNewRoom(dto);
+                if (result)
+                {
+                    return Ok(new { message = "Room created successfully" });
+                }
+                return BadRequest(new { message = "Room creation failed" });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"An error occurred: {ex.Message}" });
+            }
+        }
+
+        [HttpGet("user/{userId}")]
+        public async Task<IActionResult> GetRoomByUserId(Guid userId)
+        {
+            try
+            {
+                var rooms = await _roomService.GetRoomByUserId(userId);
+                return Ok(rooms);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, new { message = $"An error occurred: {ex.Message}" });
+            }
+        }
+
+
+
+    }
+}
